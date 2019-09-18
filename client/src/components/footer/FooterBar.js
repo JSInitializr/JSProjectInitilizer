a02bfe8f9d92e5caba8bf3e0cbbddf2622c0cf55
IshantL <ishantlambhate@gmail.com>
2019-09-18 16:18:59 +0530

    footer update

--- client/src/components/footer/FooterBar.js
+++ client/src/components/footer/FooterBar.js
@@ -5,11 +5,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
     button: {
-        alignItems:'center',
-        justifyContent:'center'
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     appBar: {
         top: 'auto',
@@ -21,16 +23,28 @@ export default function BottomAppBar() {
     const classes = useStyles();
 
     return (
-        <React.Fragment>
+        <>
             <CssBaseline />
             <AppBar position="fixed" color="primary" className={classes.appBar}>
-                <Toolbar >
-                    <Button variant="contained" color="default" >
-                        Generate the project
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                    </Grid>
+                    <Grid item xs={3} style={{textAlign: 'right'}}>
+                        <Typography variant="body2" gutterBottom>
+                            © 2019 JS Project Initializer.<br/>
+                            Bootstrap Quick development.
+                    </Typography>
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Toolbar >
+                            <Button variant="contained" color="default" >
+                                Generate the project
                         <CloudDownloadIcon />
-                    </Button>
-                </Toolbar>
+                            </Button>
+                        </Toolbar>
+                    </Grid>
+                </Grid>
             </AppBar>
-        </React.Fragment>
+        </>
     );
 }
