808d2b7c160f2d99776262dfcd2084c9a81a8655
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-18 18:40:49 +0530

    Added Expandable panel for options

--- client/src/components/body/ProjectMetaData.js
+++ client/src/components/body/ProjectMetaData.js
@@ -1,49 +1,72 @@
 import React from 'react';
 import BasicTextField from '../controls/BasicTextField';
+import BasicExpansionPanel from '../controls/BasicExpansionPanel';
 
 
-export default function ProjectMetaData (){
+export default function ProjectMetaData() {
 
-    
-        const inputs = {
-            metaData:[
-                {
-                  label:'Description',
-                  placeholder:'Enter Text',
-                  value:''
-                },
-                {
-                  label:'Git repository link',
-                  placeholder:'Enter Text',
-                  value:''
-                },
-                {
-                  label:'Keywords',
-                  placeholder:'Enter Text',
-                  value:''
-                },
-                {
-                  label:'Author',
-                  placeholder:'Enter Text',
-                  value:''
-                },
-                {
-                  label:'License',
-                  placeholder:'Enter Text',
-                  value:''
-                }
-            ]
-        }
 
-    
-        const options = inputs.metaData.map((data)=>{
-            return (<>
-              <BasicTextField label={data.label} placeholder={data.placeholder} value={data.value}/>
-              </>);
-        });
-    
-        return options;
-    
+    const inputs = {
+        optionalInputs: [
+            {
+                label: 'Description',
+                placeholder: 'Enter Text',
+                value: ''
+            },
+            {
+                label: 'Git repository link',
+                placeholder: 'Enter Text',
+                value: ''
+            },
+            {
+                label: 'Keywords',
+                placeholder: 'Enter Text',
+                value: ''
+            },
+            {
+                label: 'Author',
+                placeholder: 'Enter Text',
+                value: ''
+            },
+            {
+                label: 'License',
+                placeholder: 'Enter Text',
+                value: ''
+            }
+        ],
+        requiredInputs:[
+            {
+                label: 'PackageName',
+                placeholder: '',
+                value: ''
+            },
+            {
+                label: 'Version',
+                placeholder: '',
+                value: ''
+            },
+        ]
+    }
+
+
+    const requiredInputs = inputs.requiredInputs.map((data) => {
+        return (<>
+            <BasicTextField label={data.label} placeholder={data.placeholder} value={data.value} />
+        </>);
+    });
+
+    const optionalInputs = inputs.optionalInputs.map((data) => {
+        return (<>
+            <BasicTextField label={data.label} placeholder={data.placeholder} value={data.value} />
+        </>);
+    });
+
+    return (<>
+        {requiredInputs}
+        <BasicExpansionPanel detailPanel={optionalInputs}/>
+    </>
+    );
+
 }
 
 
