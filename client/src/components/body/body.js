907c1dcccbf411d4eda268e6c6fa7b9892d74189
IshantL <ishantlambhate@gmail.com>
2019-09-19 18:23:01 +0530

    added dependency rendering logic

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import ProjectMetaData from "./ProjectMetaData";
 import PrimarySelectionTabs from "./PrimarySelectionTabs";
-import Card from './Card';
+import Dependencies from "./Dependencies";
 
 class Body extends Component {
   constructor(props) {
@@ -21,12 +21,13 @@ class Body extends Component {
         <Grid container spacing={8}>
           <Grid item xs={12}></Grid>
           <PrimarySelectionTabs handleTabChange={(event,newValue) => this.handleTabChange(event,newValue)} shouldExpanded={this.state.shouldExpanded}/>
-          <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Project Meta Data'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Grid item xs={9} sm={6}><ProjectMetaData /></Grid></Grid>
-          <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Card/></Grid>
-          <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Card/></Grid>
+          <Grid item xs={3} sm={0}> <h4 style={{ textAlign: 'right' }}>{'Project Meta Data'}</h4></Grid>
+          <Grid item xs={6} sm={0}><ProjectMetaData /></Grid>
+          <Grid item xs={3} sm={0}></Grid>
+          <Grid item xs={3} sm={0}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
+          <Grid item xs={9} sm={0}><Dependencies/></Grid>
+          <Grid item xs={3} sm={0}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
+          <Grid item xs={9} sm={0}>end</Grid>
         </Grid>
       </>
     );
