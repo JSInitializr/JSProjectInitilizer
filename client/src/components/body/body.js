132d941c7573a0e433c9c1e194cf838c79edec81
IshantL <ishantlambhate@gmail.com>
2019-09-19 16:05:20 +0530

    added card

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import ProjectMetaData from "./ProjectMetaData";
 import PrimarySelectionTabs from "./PrimarySelectionTabs";
+import Card from './Card';
+
 class Body extends Component {
   constructor(props) {
     super(props);
@@ -26,7 +28,9 @@ class Body extends Component {
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Project Meta Data'}</h4></Grid>
           <Grid item xs={12} sm={9}><Grid item xs={9} sm={6}><ProjectMetaData /></Grid></Grid>
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><ProjectMetaData /></Grid>
+          <Grid item xs={12} sm={9}><Card/></Grid>
+          <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
+          <Grid item xs={12} sm={9}><Card/></Grid>
         </Grid>
       </>
     );
