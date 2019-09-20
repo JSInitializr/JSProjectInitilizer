40e5651b401e4a98afa7c0067a74aa96e2f6b962
IshantL <ishantlambhate@gmail.com>
2019-09-20 19:27:32 +0530

    resolved the build error and map issue

--- client/src/components/body/Dependencies.js
+++ client/src/components/body/Dependencies.js
@@ -4,9 +4,9 @@ import DependencyList from '../../assets/DependencyList';
 import Grid from "@material-ui/core/Grid";
 
 export default function Dependencies() {
-    return (<><Grid container spacing={8}>
-        <Grid item xs={12}></Grid>
-        {DependencyList.developerTools.map(t=><Grid item xs={4} sm={0}><Card /></Grid>)}
+    return (<>
+    <Grid>
+        {DependencyList('react').map(t => <Card label={t.label} desc={t.desc}/>)}
     </Grid>
     </>);
 }
--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import ProjectMetaData from "./ProjectMetaData";
 import PrimarySelectionTabs from "./PrimarySelectionTabs";
-import Card from './Card';
-import DependencyList from "../../assets/DependencyList";
 import Dependencies from "./Dependencies";
 
 class Body extends Component {
   constructor(props) {
     super(props);
-    // this.handleTabChange = this.handleTabChange.bind(this);
     this.state = { shouldExpanded: false };
-    DependencyList("hello");
   }
 
   handleTabEvent = (key,value) => {
@@ -29,9 +25,8 @@ class Body extends Component {
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Project Meta Data'}</h4></Grid>
           <Grid item xs={12} sm={9}><Grid item xs={9} sm={6}><ProjectMetaData /></Grid></Grid>
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Card /></Grid>
-          <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Card /></Grid>
+          <Grid item xs={12} sm={9}><Dependencies/></Grid>
+          <Grid item xs={12}></Grid>
         </Grid>
       </>
     );
