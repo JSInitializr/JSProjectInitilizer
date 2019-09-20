a56b2b8c3f84f7c890b9561d2ebed9cf13ab1271
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-20 10:45:53 +0530

    Tab event handler added

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -3,32 +3,30 @@ import Grid from "@material-ui/core/Grid";
 import ProjectMetaData from "./ProjectMetaData";
 import PrimarySelectionTabs from "./PrimarySelectionTabs";
 import Card from './Card';
+import DependencyList from "../../assets/DependencyList";
 
 class Body extends Component {
   constructor(props) {
     super(props);
-    this.handleTabChange = this.handleTabChange.bind(this);
+    // this.handleTabChange = this.handleTabChange.bind(this);
     this.state = { shouldExpanded: false };
-    
-    debugger;
+    DependencyList("hello");
   }
 
-  handleTabChange = (event,newValue) => {
-    this.setState({ ...this.state, shouldExpanded: (newValue === 3)});
-  }
+  
 
   render() {
     return (
       <>
         <Grid container spacing={8}>
           <Grid item xs={12}></Grid>
-          <PrimarySelectionTabs handleTabChange={(event,newValue) => this.handleTabChange(event,newValue)} shouldExpanded={this.state.shouldExpanded}/>
+          <PrimarySelectionTabs handleTabChange={(event, newValue) => this.handleTabChange(event, newValue)} shouldExpanded={this.state.shouldExpanded} />
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Project Meta Data'}</h4></Grid>
           <Grid item xs={12} sm={9}><Grid item xs={9} sm={6}><ProjectMetaData /></Grid></Grid>
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Card/></Grid>
+          <Grid item xs={12} sm={9}><Card /></Grid>
           <Grid item xs={12} sm={3}> <h4 style={{ textAlign: 'right' }}>{'Dependencies'}</h4></Grid>
-          <Grid item xs={12} sm={9}><Card/></Grid>
+          <Grid item xs={12} sm={9}><Card /></Grid>
         </Grid>
       </>
     );
