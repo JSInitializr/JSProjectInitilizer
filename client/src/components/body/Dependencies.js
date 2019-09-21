94dc660974a83e20a70c7509c254a7265629b2c1
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-21 23:26:07 +0530

    Passed state data to dependency

--- client/src/components/body/Dependencies.js
+++ client/src/components/body/Dependencies.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './Card';
-import DependencyList from '../../assets/DependencyList';
 import Grid from "@material-ui/core/Grid";
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -8,40 +7,71 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { connect } from 'react-redux';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
+class Dependencies extends Component {
 
+  constructor(props) {
+    super(props);
+  }
 
-export default function Dependencies() {
-const classes = useStyles();
-const arr =[];
-    for ( const topic in DependencyList('react')){
-        arr.push(<>
-        <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>{topic}</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-        <Grid container spacing={2}>
-          { DependencyList('react')[topic].map(t =>  <Grid  item xs={4} sm={0}><Card label={t.label} desc={t.desc}/></Grid>)} 
-          </Grid>   
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      </>)
+  useStyles = () => {
+    return makeStyles(theme => ({
+      root: {
+        width: '100%',
+      },
+      heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+      },
+    }))
+  };
 
+  dependencyList = (tag) => {
+    let newPackages = {};
+    for (const category in this.props.dependencyList) {
+      const pkgs = this.props.dependencyList[category];
+      const filteredPkgs = pkgs.filter((pkg) => {
+        return pkg.tag && pkg.tag.indexOf(tag) > -1;
+      });
+      newPackages[category] = filteredPkgs;
     }
-   return arr;
+    return newPackages;
+  }
+
+
+  render() {
+
+    const classes = this.useStyles();
+    const arr = [];
+    for (const topic in this.dependencyList('react')) {
+      arr.push(<>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>{topic}</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Grid container spacing={2}>
+              {this.dependencyList('react')[topic].map(t => <Grid item xs={4} sm={0}><Card label={t.label} desc={t.desc} /></Grid>)}
+            </Grid>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </>)
+    }
+    return (<>
+      {arr}
+    </>)
+  }
+
 }
-        
+
+
+const mapStateToProps = (state) => ({
+  dependencyList: state.response.dependencyList,
+});
+
+export default connect(mapStateToProps, null)(Dependencies);
