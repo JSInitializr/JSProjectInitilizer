7575939dbf5d86c9890db86d172a905d11be2523
IshantL <ishantlambhate@gmail.com>
2019-09-21 14:33:32 +0530

    refactor

--- client/src/components/controls/BasicTab.js
+++ client/src/components/controls/BasicTab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
