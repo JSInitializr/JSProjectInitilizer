e4bcc2914898f9e7d14335096b223b16090b5edc
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-23 00:11:21 +0530

    Dynamically loaded controls from ui.json file url

--- client/src/redux/sagas.js
+++ client/src/redux/sagas.js
@@ -1,16 +1,15 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
-import { fetchUIRenderData } from './actions';
-function* submitFormData() {
-    const json = yield fetch('To do: api to submit form data')
+
+function* fetchUIControlsData() {
+    const json = yield fetch('https://raw.githubusercontent.com/JSInitializr/JSProjectInitilizer/master/client/src/assets/ui.json')
         .then(response => response.json());
-    yield put({ type: "RECEIVED_UI_CONTROLS", json: json.message, });
+    yield put({ type: "RECEIVED_UI_CONTROLS", response: json.response, });
 }
 
-function* actionWatcherForFetchUIControls(){
-    yield takeLatest('FETCH_UI_CONTROLS',fetchUIControlsData)
+function* actionWatcherForFetchUIControls() {
+    yield takeLatest('FETCH_UI_CONTROLS', fetchUIControlsData)
 }
-
 
 export default function* rootSaga() {
-    yield all([actionWatcherForFetchUIControls(),actionWatcherForFetchDependencyList]);
+    yield all([actionWatcherForFetchUIControls(),]);
 }
