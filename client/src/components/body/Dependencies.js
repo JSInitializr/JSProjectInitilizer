6ac5dac21bbeebd3f725a7a9d4ba9bca73b8b14a
sachin.thakur <Sachin.Thakur@allocatesoftware.com>
2019-09-23 16:23:42 +0530

    Added BasicExpansionPanel to dependency component

--- client/src/components/body/Dependencies.js
+++ client/src/components/body/Dependencies.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import Grid from "@material-ui/core/Grid";
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import BasicExpansionPanel from '../controls/BasicExpansionPanel';
+import DetailMore from '../controls/DetailMore';
 
 class Dependencies extends Component {
 
@@ -35,37 +32,38 @@ class Dependencies extends Component {
   }
 
 
-  render() {
-     if(!this.props.dependencyList){
-       return(<div><h1>No list here</h1></div>)
-     }
+  detailMoreOptionControl = (topic) => {
+    return <DetailMore label={topic}/>
+  }
 
-    const classes = this.useStyles();
+  getDetailPanel = (topic) => {
+    return (<>
+      <Grid container spacing={2}>
+        {this.dependencyList('react')[topic].map(t => <Grid key={t.label} item xs={4} sm={0}><Card label={t.label} desc={t.desc} /></Grid>)}
+      </Grid>
+    </>);
+
+  }
+
+
+  render() {
+
+    if (!this.props.dependencyList) {
+      return (<div><h1>No list here</h1></div>)
+    }
+
     const arr = [];
     for (const topic in this.dependencyList('react')) {
       arr.push(<>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className={classes.heading}>{topic}</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Grid container spacing={2}>
-              {this.dependencyList('react')[topic].map(t => <Grid key= {t.label} item xs={4} sm={0}><Card label={t.label} desc={t.desc} /></Grid>)}
-            </Grid>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
+        <BasicExpansionPanel summaryPanel={this.detailMoreOptionControl(topic)} detailPanel={this.getDetailPanel(topic)} />
       </>)
     }
     return (<>
       {arr}
-    </>)
+    </>);
   }
 }
 
 
 
-export default Dependencies;
+  export default Dependencies;
