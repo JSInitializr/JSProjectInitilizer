39132d095777fa84896c27eeb8e4d467dcf5ec88
IshantL <ishantlambhate@gmail.com>
2019-09-23 11:40:05 +0530

    functional component

--- client/src/components/controls/BasicTab.js
+++ client/src/components/controls/BasicTab.js
@@ -1,54 +1,57 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
+function a11yProps(id,index) {
+  return {
+    id: `${id}`,
+    'aria-controls': `action-tabpanel-${index}`,
+  };
+}
 
-class BasicTab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: 0 };
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 800
+  },
+  tab: {
+    maxWidth: 50,
+    minWidth: 20,
+    position: "relative"
+  }
+}));
+
+export default function BasicTab(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+    if(props.handleChange){
+      props.handleChange(event,newValue);
+    }
   }
 
-  a11yProps = (id,index) => {
-    return {
-      id: `${id}`,
-      'aria-controls': `action-tabpanel-${index}`,
-    };
-  }
-  setValue = (tab) =>{
-    console.log(tab);
-  }
-  tabs = () => {
-    const t = this.props.tabs.map((tab) => {
-      return <Tab label={tab} {...this.a11yProps(this.props.tabTitle,0)} />
+
+  const tabs = () => {
+    const t = props.tabs.map((tab) => {
+      return <Tab label={tab} {...a11yProps(props.tabTitle,0)} />
     })
     return t;
   }
 
-  handleChange = (event, newValue) => {
-    this.setState({value:newValue});
-    if(this.props.handleChange){
-      this.props.handleChange(event,newValue);
-    }
-  }
+  return (
+    <div className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        aria-label="action tabs example"
+      >
+        {tabs()}
+      </Tabs>
 
-  render(){
-    return (
-      <div >
-        <Tabs
-          value={this.state.value}
-          onChange={this.handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          aria-label="action tabs example"
-        >
-          {this.tabs()}
-        </Tabs>
-  
-      </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default BasicTab;
