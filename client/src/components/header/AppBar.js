49954f62ab0c1a7f5d633b35020c8bdb8bc1645d
IshantL <ishantlambhate@gmail.com>
2019-09-25 15:12:40 +0530

    refactor appbar

--- client/src/components/header/AppBar.js
+++ client/src/components/header/AppBar.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import logo from '../../assets/js.png';
 import Switch from './switch';
 import Help from './helpMenu';
 
