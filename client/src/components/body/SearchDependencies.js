6f56751b8fddaa960628c3028ea110bdf2d4cc28
IshantL <ishantlambhate@gmail.com>
2019-09-25 15:28:29 +0530

    converted functional to class based component

--- client/src/components/body/SearchDependencies.js
+++ client/src/components/body/SearchDependencies.js
@@ -1,21 +1,25 @@
-import React, { useState }from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component }from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
-
 const axios = require('axios');
-
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
-
 let size = 10,apiLink='';
 
-const searchDependency = () =>{
+class SearchDependency extends Component{
+
+ state={
+  searchText:'',
+  dependencies:''
+ }
+
+dependencyText =(event) =>{
+  this.setState({searchText:event.target.value});
+  apiLink =`https://api.npms.io/v2/search?q=${this.state.searchText}&size=${size}`
+  //console.log(event.target.value);
+}
+
+searchDependency = () =>{
   axios.get(apiLink)
   .then(function (response) {
     // handle success
@@ -30,32 +34,23 @@ const searchDependency = () =>{
     console.log("end")
   });
 }
-
-
-
-export default function InputWithIcon(props) {
-  const [searchText, setText] = useState('');
-  const classes = useStyles();
-
-
-  const dependencyText =(event) =>{
-    setText(event.target.value);
-    apiLink =`https://api.npms.io/v2/search?q=${searchText}&size=${size}`
-    //console.log(event.target.value);
-  }
-
+ render(){
   return (
-   <div className={classes.margin}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-          <TextField id="input-with-icon-grid" label="Search dependencies" onChange={dependencyText}/>
-          </Grid>
-          <Grid item>
-          <Button variant="contained" color="secondary" className={classes.button} onClick={searchDependency}>
-        <SearchIcon className={classes.rightIcon} />
-      </Button>
-          </Grid>
-        </Grid>
-      </div>
-  );
+    <div >
+         <Grid container spacing={1} alignItems="flex-end">
+           <Grid item>
+           <TextField id="input-with-icon-grid" label="Search dependencies" onChange={this.dependencyText}/>
+           </Grid>
+           <Grid item>
+           <Button variant="contained" color="secondary"  onClick={this.searchDependency}>
+         <SearchIcon />
+       </Button>
+           </Grid>
+         </Grid>
+       </div>
+   );
+ }
 }
+export default SearchDependency;
+
+
