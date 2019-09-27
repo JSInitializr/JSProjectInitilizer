b8a9b635b4a1f08a88abf829782fc594c630ad31
Pankaj Purke <pankaj.purke@synapse.com>
2019-09-27 16:36:15 +0530

    UI refactoring

---
+++ src/components/controls/BasicExpansionPanel.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        boxShadow: 'none',
+        background: 'none'
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
+
+export default function BasicExpansionPanel(props) {
+    const classes = useStyles();
+    var sliderRefObj;
+
+    function sliderRef(node) {
+        sliderRefObj = node;
+    }
+
+    const handleChange = panel => (event, expanded) => {
+        setTimeout(function () {
+            const nextFocusableElements = sliderRefObj && sliderRefObj.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+            if (nextFocusableElements && nextFocusableElements.length > 0) {
+                const f = nextFocusableElements[0];
+                f.focus();
+            }
+        }, 10);
+    };
+   
+
+
+    return (
+        <div >
+            <ExpansionPanel defaultExpanded={props.defaultExpanded}className={classes.root} expanded={props.expanded} onChange={handleChange('panel1').bind(this)}>
+                <ExpansionPanelSummary
+                    style={{minHeight:'57px', height:'57px',padding: '0px 0px 0px' }}>
+                    {props.summaryPanel}
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails ref={sliderRef} style={{ display: 'inline', padding: '0px 0px 0px' }}>
+                    {props.detailPanel}
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        </div>
+    );
+}
---
+++ src/components/layouts/TopNavigation.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Switch from './switch';
+import Logo from './Logo';
+import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import Box from '@material-ui/core/Box';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import Help from './helpMenu';
+
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1,
+    color:'#222',
+  },
+  menuButton: {
+    marginRight: theme.spacing(0.5),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  appBarStyle:{
+    backgroundColor: 'initial',
+    color:'#222',
+    boxShadow:'none',
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+}));
+export default function TopNavigation() {
+  const classes = useStyles();
+
+ 
+
+  return (
+    <div className={classes.grow}>
+     <AppBar position="static" className={classes.appBarStyle}>
+        <Box p={1}>
+            <Grid container  md={12} lg={12}>
+                <Grid item xs={3} md={3} style={{alignSelf: 'flex-end'}} >
+                <div>
+                    <Logo/>
+                </div>
+                </Grid>
+                <Grid item  xs={9} md={9} >
+                <Box display="flex" justifyContent="flex-end" style={{marginTop: '1%'}} >
+                <Switch/>
+                    <Button color="inherit" className={classes.menuButton}>Github</Button>
+                    <Help/>
+                </Box>
+                </Grid>    
+            </Grid>
+        </Box>
+     </AppBar>
+    </div>
+  );
+}
