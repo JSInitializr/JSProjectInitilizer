4daa095ca8078472dc79d6b1d8f64b3ea7320455
IshantL <ishantlambhate@gmail.com>
2019-10-01 11:17:44 +0530

    added toggle button theme change

--- src/components/layouts/switch.js
+++ src/components/layouts/switch.js
@@ -5,19 +5,24 @@ import Switch from '@material-ui/core/Switch';
 
 export default function SwitchLabels() {
   const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
+    light: false
   });
 
   const handleChange = name => event => {
     setState({ ...state, [name]: event.target.checked });
+    console.log(state);
+    if( state.light){
+      document.getElementById('body').className ="light"
+    }else{
+      document.getElementById('body').className ="dark"
+    }
   };
 
   return (
     <FormGroup row>
       <FormControlLabel
         control={
-          <Switch checked={state.checkedA} onChange={handleChange('checkedA')} value="checkedA" />
+          <Switch checked={state.checkedA} onChange={handleChange('light')} value="light" />
         }
         label="Color"
       />
